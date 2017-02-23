2c9a13231c5d5a3fe7b147e3fe50ddeaff277679
Daniel McDonald <mcdonadt@colorado.edu>
2017-02-23 12:29:13 -0800

    Only query with the holdouts

--- run_sortmerna.sh
+++ run_sortmerna.sh
@@ -5,9 +5,13 @@ threads=$1
 for q in *.queries
 do
     qbase=$(basename ${q} .queries)
+    grep "^>" ${q} | sort - > .qids
     for f in *.fasta
     do
+        grep "^>" ${f} | sort - > .fids
+        grep -w -v -A 1 -F -f .fids .qids | grep -v "\-\-" > .holdoutids  # --no-group-separator isn't on BSD grep :/
+        grep -w -A 1 -F -f .holdoutids ${q} | grep -v "\-\-" > .query_holdouts
         fbase=$(basename ${f} .fasta)
-        sortmerna --reads ${q} --ref ${f},${fbase}.idx --aligned ${qbase}-${fbase} --blast 3 --best 1 --print_all_reads -v -a ${threads}
+        sortmerna --reads .query_holdouts --ref ${f},${fbase}.idx --aligned ${qbase}-${fbase} --blast 3 --best 1 --print_all_reads -v -a ${threads}
     done
 done
